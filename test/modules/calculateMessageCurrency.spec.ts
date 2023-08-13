2f49f549f4b68d2adfaaf405c6364cf74b48a586
Naomi Carrigan <nhcarrigan@gmail.com>
2023-08-13 10:03:09 -0700

    feat: increase gain rates

--- test/modules/calculateMessageCurrency.spec.ts
+++ test/modules/calculateMessageCurrency.spec.ts
@@ -6,16 +6,16 @@ suite("calculateMessageCurrency module", () => {
   test("should accurately calculate the currency of a message under 10 characters", () => {
     for (let i = 0; i < 1000; i++) {
       const total = calculateMessageCurrency("Naomi");
-      assert.isAtMost(total, 10);
-      assert.isAtLeast(total, 1);
+      assert.isAtMost(total, 100);
+      assert.isAtLeast(total, 50);
     }
   });
 
   test("should accurately calculate the currency of a message at 25 characters", () => {
     for (let i = 0; i < 1000; i++) {
       const total = calculateMessageCurrency("NaomiNaomiNaomiNaomiNaomi");
-      assert.isAtMost(total, 20);
-      assert.isAtLeast(total, 2);
+      assert.isAtMost(total, 200);
+      assert.isAtLeast(total, 100);
     }
   });
 
@@ -24,8 +24,8 @@ suite("calculateMessageCurrency module", () => {
       const total = calculateMessageCurrency(
         "NaomiNaomiNaomiNaomiNaomiNaomiNaomiNaomiNaomiNaomi"
       );
-      assert.isAtMost(total, 50);
-      assert.isAtLeast(total, 5);
+      assert.isAtMost(total, 500);
+      assert.isAtLeast(total, 250);
     }
   });
 });
