e6f1f364c7969cbacc888df72967cb5cf9ebaaa9
Naomi Carrigan <nhcarrigan@gmail.com>
2023-08-12 22:00:28 -0700

    feat: add about command

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -1,5 +1,8 @@
-import { SlashCommandBuilder } from "discord.js";
+import { users } from "@prisma/client";
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 
+import { CurrencyValues } from "../config/CurrencyValues";
+import { Emotes } from "../config/Emotes";
 import { Command } from "../interfaces/Command";
 import { errorHandler } from "../utils/errorHandler";
 
@@ -9,8 +12,28 @@ export const about: Command = {
     .setDescription("About the currency system"),
   run: async (bot, interaction) => {
     try {
+      const totals = (
+        Object.entries(CurrencyValues) as [keyof users["currency"], number][]
+      ).map(
+        ([currency, value]) =>
+          `One ${
+            Emotes[currency]
+          } is worth ${value.toLocaleString()} NaomiCoin.`
+      );
+
+      const embed = new EmbedBuilder();
+      embed.setTitle("NaomiCoin");
+      embed.setDescription(
+        "Our currency system is designed to reward activity in our community. You earn currency slowly as you interact with your fellow members, and can be granted currency by Naomi for specific events and rewards.\n\nYou can use your currency to purchase items with the `/purchase` command. If you want to know about a specific item, you can look it up with the `/item` command. See how much you currently have with the `/wallet` command!\n\nCurrencies have no cash value."
+      );
+      embed.addFields([
+        {
+          name: "Currency values",
+          value: totals.join("\n"),
+        },
+      ]);
       await interaction.editReply({
-        content: "This is a command.",
+        embeds: [embed],
       });
     } catch (err) {
       await errorHandler(bot, "about command", err);
