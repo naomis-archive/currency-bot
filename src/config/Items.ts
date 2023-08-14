fdd178732367800595e4bc7876e7abec9250cb5c
Naomi Carrigan <nhcarrigan@gmail.com>
2023-08-14 11:05:38 -0700

    feat: add date item (#8)

--- src/config/Items.ts
+++ src/config/Items.ts
@@ -63,4 +63,11 @@ export const Items: Item[] = [
     price: 100000000,
     internalId: "hall-of-fame",
   },
+  {
+    name: "Go on a 'Date' with Naomi",
+    description:
+      "You get an hour of time to vibe with Naomi, but you have to do the planning. You could play games, pair on some code, get your resume or project reviewed, and more! Must be family-friendly and appropriate for our community.",
+    price: 250000000,
+    internalId: "date",
+  },
 ];
