2681480c5690e7c6e646831af7eb1b8d70a7a084
Naomi Carrigan <nhcarrigan@gmail.com>
2023-08-13 17:12:35 -0700

    feat: add slots command (#4)

---
+++ src/commands/slots.ts
@@ -0,0 +1,81 @@
+import { SlashCommandBuilder } from "discord.js";
+
+import { CurrencyName } from "../config/CurrencyName";
+import { SlotReel, Slots } from "../config/Slots";
+import { Command } from "../interfaces/Command";
+import { makeChange } from "../modules/makeChange";
+import { sumCurrency } from "../modules/sumCurrency";
+import { errorHandler } from "../utils/errorHandler";
+import { getDataRecord } from "../utils/getDataRecord";
+import { getRandomValue } from "../utils/getRandomValue";
+import { sleep } from "../utils/sleep";
+
+export const slots: Command = {
+  data: new SlashCommandBuilder()
+    .setName("slots")
+    .setDescription(`Gamble your ${CurrencyName} on the slot machines!`)
+    .addIntegerOption((option) =>
+      option
+        .setName("wager")
+        .setDescription("How much do you want to bet?")
+        .setRequired(true)
+    ),
+  run: async (bot, interaction) => {
+    try {
+      const wager = interaction.options.getInteger("wager", true);
+      const userRecord = await getDataRecord(bot, interaction.user.id);
+      if (!userRecord) {
+        await interaction.editReply({
+          content: "There was an error with the database. Please try again.",
+        });
+        return;
+      }
+      const oldTotal = sumCurrency(userRecord.currency);
+      if (wager > oldTotal) {
+        await interaction.editReply({
+          content: `You can't wager more ${CurrencyName} than you have!`,
+        });
+        return;
+      }
+      const first = getRandomValue(Slots);
+      const second = getRandomValue(Slots);
+      const third = getRandomValue(Slots);
+      const set = new Set([first, second, third]);
+      const won = set.size < 3;
+      // lose if three different, if two match get 10x, if three match get 100x
+      const result =
+        set.size >= 3 ? 0 - wager : set.size === 2 ? wager * 10 : wager * 100;
+      await interaction.editReply({
+        content: `Spinning...\n# ${SlotReel} ${SlotReel} ${SlotReel}`,
+      });
+      await sleep(1000);
+      await interaction.editReply({
+        content: `Spinning...\n# ${first} ${SlotReel} ${SlotReel}`,
+      });
+      await sleep(1000);
+      await interaction.editReply({
+        content: `Spinning...\n# ${first} ${second} ${SlotReel}`,
+      });
+      await sleep(1000);
+      await interaction.editReply({
+        content: `Spinning...\n# ${first} ${second} ${third}`,
+      });
+      const newTotal = oldTotal + result;
+      await bot.db.users.update({
+        where: {
+          userId: interaction.user.id,
+        },
+        data: {
+          currency: { ...makeChange(newTotal) },
+        },
+      });
+      await interaction.editReply({
+        content: `You ${won ? "won" : "lost"} ${Math.abs(
+          result
+        ).toLocaleString()} ${CurrencyName}!\n# ${first} ${second} ${third}`,
+      });
+    } catch (err) {
+      await errorHandler(bot, "slots command", err);
+    }
+  },
+};
---
+++ src/config/Slots.ts
@@ -0,0 +1,22 @@
+/**
+ * Emotes to use in the slot machines.
+ */
+export const Slots = [
+  "<:Naomato:1065883670537318400>",
+  "<:NaomiBlush:1065883706000158770>",
+  "<:NaomiCheer:1065884155273023598>",
+  "<:NaomiGiggle:1065884641116049478>",
+  "<:NaomiGrin:1065884732119859200>",
+  "<:NaomiInnocent:1065885047950942218>",
+  "<:NaomiKiss:1065885121481281556>",
+  "<:NaomiPat:1065885244022071386>",
+  "<:NaomiWink:1065906272072900628>",
+  "<:NaomiCute:1065884248957009921>",
+  "<:NaomiYawn:1065906305501511710>",
+  "<:NaomiWoah:1065906289202446376>",
+  "<:NaomiSing:1065885810785796126>",
+  "<:NaomiSmh:1065885897775644732>",
+  "<:NaomiThink:1065906168414863360>",
+];
+
+export const SlotReel = "<a:slot:1140435469478404208>";
